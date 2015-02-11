d9593783c15861c5a81feae620a805fa0afb87a3
Liam Svanåsbakken Crouch <petterroea@skymiastudios.com>
2015-02-11 23:33:56 +0100

    Updated infopage

---
+++ scripts/infoscreen.js
@@ -0,0 +1,112 @@
+var contentsArray = new Array();
+var slideAt = 0;
+var nextImage = "";
+var imageArray = new Array("BF01", "BF02", "BF03", "BF04", "cod01", "cod02", "cod03", "cod04", "ghost_recon01", "MOH01", "sniper01");
+var version_id = 2; //Used for updating live
+
+$(document).ready(function() {
+	setInterval(function() {
+		updateSlides();
+	}, 1000*15);
+	
+	setInterval(function() {
+		updateImage();
+	}, 1000*5*60);
+	
+	prepareNextImage(); //Preload next image
+	updateImage();
+	loadPage();
+});
+
+function loadPage() {
+	slideAt = 0;
+	// Get json for agenda
+	$.getJSON('api/json/agenda/getPublishedNotHappendAgendas.php', function(data) {	
+		if (data.agendaList.length >= 1) {
+			$("#agendaContainer").empty();
+			$.each(data.agendaList, function(index, agenda) {
+				if (agenda.isHappening) {
+					$('#agendaContainer').append('<div class="happeningAgenda">' + 
+						'<h3>' + agenda.startTime + '</h3>' + 
+						'<h1><b>' + agenda.title + '</b></h1>' +
+						'<h4>' + agenda.description + '</h4>' +
+					'</div>');
+				} else {
+					$('#agendaContainer').append('<div class="pendingAgenda">' + 
+						'<h3>' + agenda.startTime + '</h3>' + 
+						'<h1><b>' + agenda.title + '</b></h1>' +
+						'<h4>' + agenda.description + '</h4>' +
+					'</div>');
+				}
+			});
+		}
+	});
+	
+	// Get JSON for slides
+	$.getJSON('api/json/slide/getPublishedSlides.php', function(data) {
+		$.each(data.slides, function(index, slide) {
+			contentsArray.push('<h1>' + slide.title + "</h1>" + slide.content);
+		});
+	});
+	updateSlides();
+}
+
+//Image handling
+function preloadImage (url) {
+	try {
+		var _img = new Image();
+		_img.src = url;
+	} catch (e) {
+		
+	}
+}
+
+function prepareNextImage() {
+	var r = Math.floor((Math.random()*(imageArray.length - 1)) + 1);
+	nextImage = "../images/backgrounds/"+ imageArray[r] +".jpg";
+	preloadImage(nextImage);
+}
+
+function updateImage() {
+	//Change background
+	$("html").css('background-image', 'url('+ nextImage +')');
+	prepareNextImage();
+}
+
+function updateSlides() {	
+	var shouldUpdate = false;
+	
+	//Fix for less repetition
+	if(contentsArray.length % 2 == 0) { //Is it an even number?
+		if(slideAt == ( contentsArray.length *2) - 2) {
+			shouldUpdate = true;
+		}
+	} else {
+		if(slideAt == (contentsArray.length*2)) {
+			shouldUpdate = true;
+		}
+	}
+	
+	if (shouldUpdate) {
+		$.getJSON('version.txt', function(data) {	
+			if(data.version != version_id) {
+				location.reload();
+			} else {
+				loadPage();
+			}
+		});
+	} else {
+		$("#slideContainer").fadeOut('slow', function(){
+			$("#slideContainer").empty();
+			$("#slideContainer").append(contentsArray[(slideAt)%contentsArray.length]);
+			$("#slideContainer").fadeIn('slow');
+		});
+		
+		$("#slideContainerTwo").fadeOut('slow', function(){
+			$("#slideContainerTwo").empty();
+			$("#slideContainerTwo").append(contentsArray[(slideAt+1)%contentsArray.length]);
+			$("#slideContainerTwo").fadeIn('slow');
+		});
+		slideAt += 2;
+	}
+}
